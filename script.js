532066c89cd3839221ff207549f67f55a961dd4b
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-01-19 13:05:09 +0000

    Switch add class to body & transition

---
+++ script.js
@@ -0,0 +1,9 @@
+let styles = document.getElementsByName('style');
+let styleLink = document.getElementById('stylelink');
+
+styles.forEach((style) => {
+    style.addEventListener('change', () => {
+        styleLink.className = style.id.toString();
+        console.log(style.id);
+    })
+})
